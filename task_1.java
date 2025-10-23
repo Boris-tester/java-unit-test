4ed399ee7bb481487846c18f704a105d1d26f1e3
Boris-tester <boris001amg@yandex.ru>
2025-10-24 00:28:36 +0300

    Update task_1.java
    Add test for age > 18

---
+++ task_1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Task1Test {
+
+    @Test
+    public void checkIsAdultWhenAgeIsMoreThan18True() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(19);
+        assertEquals("Если возраст > 18, метод должен вернуть true", true, isAdult);
+    }
+}
